4ea0c93dabaccd9c9e82cd9c6b05f9379072acfd
tutor0613 <tutor0613@github.com>
2022-02-04 20:33:26 +0900

    220204 : Add srv server in calculator

--- basic_pkg/basic_pkg/calculator.py
+++ basic_pkg/basic_pkg/calculator.py
@@ -3,6 +3,7 @@ from rclpy.node import Node
 from rclpy.qos import QoSProfile, QoSDurabilityPolicy, QoSHistoryPolicy, QoSReliabilityPolicy
 from rclpy.callback_groups import ReentrantCallbackGroup
 from basic_msgs.msg import TwoInt32
+from basic_msgs.srv import OperateArgs
 
 
 class Calculator(Node):
@@ -10,6 +11,8 @@ class Calculator(Node):
         super().__init__('calculator')
 
         self.callback_group = ReentrantCallbackGroup()  # Multi-thread callback
+        self.arg_a = 0
+        self.arg_b = 0
 
         qos_profile = QoSProfile(
             reliability = QoSReliabilityPolicy.RELIABLE,
@@ -23,9 +26,10 @@ class Calculator(Node):
             callback_group=self.callback_group
         )
 
-        # self.operate_srv_server = self.create_service(
-            
-        # )
+        self.operate_srv_server = self.create_service(
+            OperateArgs, 'operate_cmd', self.operate_cmd_callback,
+            callback_group=self.callback_group
+        )
 
     def argument_callback(self, msg):
         try:
@@ -36,6 +40,19 @@ class Calculator(Node):
             self.get_logger().warn("[Calculator] Exception : %s"%e)
             return
 
+    def operate_cmd_callback(self, req, resp):
+        try:
+            operator = req.cmd
+            a = self.arg_a; b = self.arg_b
+            formula = "%d %s %d"%(a, operator, b)
+            resp.result = eval(formula)
+
+            self.get_logger().warn("[Calculator] %s = %d"%(formula, resp.result))
+            return resp
+
+        except Exception as e:
+            self.get_logger().warn("[Calculator] Exception : %s"%e)
+            return -1
 
 
 def main(args=None):
