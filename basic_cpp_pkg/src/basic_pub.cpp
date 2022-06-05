720df1c2dba742300906c0124b080832e629b94f
tutor0613 <tutor0613@github.com>
2022-06-05 09:30:07 +0000

    220605 : Add basic_cpp_pkg

---
+++ basic_cpp_pkg/src/basic_pub.cpp
@@ -0,0 +1,42 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "basic_msgs/msg/Int32.hpp"
+
+using namespace std::chrono_literals;
+
+class BasicCppPublisher : public rclcpp::Node
+{
+    public:
+        BasicCppPublisher()
+        : Node("basic_cpp_pub"), count_(0)
+        {
+            auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+            basic_publisher_ = this->create_publisher<basic_msgs::msg::Int32>("topic", qos_profile);
+            timer_ = this->create_wall_timer(1s, std::bind(&publish_msg, this));
+        }
+    
+    private:
+        void publish_msg()
+        {
+            auto msg = basic_msgs::msg::Int32();
+            msg.data = count_++;
+            RCLCPP_WARN(this->get_logger(), "[BasicCppPub] Published : %s", msg.data.c_str());
+            basic_publisher_->publish(msg);
+        }
+        rclcpp::TimerBase::SharedPtr timer_;
+        rclcpp::Publisher<basic_msgs::msg::Int32>::SharedPtr basic_publisher_;
+        size_t count_;
+}
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<BasicCppPublisher>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
