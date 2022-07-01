8cb14dccd9e67f267169401a384d25f8f32e4043
tutor0613 <tutor0613@github.com>
2022-07-01 13:24:31 +0000

    220701 : Clear build err in basic_pub.cpp

--- basic_cpp_pkg/src/basic_pub.cpp
+++ basic_cpp_pkg/src/basic_pub.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
-#include "basic_msgs/msg/Int32.hpp"
+#include "std_msgs/msg/int32.hpp"
 
 using namespace std::chrono_literals;
 
@@ -15,22 +15,22 @@ class BasicCppPublisher : public rclcpp::Node
         : Node("basic_cpp_pub"), count_(0)
         {
             auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
-            basic_publisher_ = this->create_publisher<basic_msgs::msg::Int32>("topic", qos_profile);
-            timer_ = this->create_wall_timer(1s, std::bind(&publish_msg, this));
+            basic_publisher_ = this->create_publisher<std_msgs::msg::Int32>("topic", qos_profile);
+            timer_ = this->create_wall_timer(1s, std::bind(&BasicCppPublisher::publish_msg, this));
         }
     
     private:
         void publish_msg()
         {
-            auto msg = basic_msgs::msg::Int32();
+            auto msg = std_msgs::msg::Int32();
             msg.data = count_++;
-            RCLCPP_WARN(this->get_logger(), "[BasicCppPub] Published : %s", msg.data.c_str());
+            RCLCPP_WARN(this->get_logger(), "[BasicCppPub] Published : %d", msg.data);
             basic_publisher_->publish(msg);
         }
         rclcpp::TimerBase::SharedPtr timer_;
-        rclcpp::Publisher<basic_msgs::msg::Int32>::SharedPtr basic_publisher_;
+        rclcpp::Publisher<std_msgs::msg::Int32>::SharedPtr basic_publisher_;
         size_t count_;
-}
+};
 
 int main(int argc, char * argv[])
 {
